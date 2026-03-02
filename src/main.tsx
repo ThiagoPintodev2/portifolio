b8c4a623ee72ee02445ccf73d7eceef66b4caee9
thiago.dev2 <thiago.dev2@gmail.com>
2026-03-02 18:35:06 -0300

    adjusts in the route of nav menu

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import {  BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
 
@@ -9,5 +9,5 @@ createRoot(document.getElementById("root")!).render(
     <StrictMode>
       <App />
     </StrictMode>
-  </BrowserRouter>,
+  </BrowserRouter>
 );
